b492c156be344da04ba68140abd2c337a1dc69e4
Pablo Alonso <pepoalonso1@gmail.com>
2021-04-15 17:36:01 -0300

    agregado navbar y footer y sus rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Inicio from './components/Inicio';
+import ListarProductos from './components/ListarProductos';
+import AgregarProducto from './components/AgregarProducto';
+import Footer from './components/common/Footer';
+import NavLine from './components/common/NavLine';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+   <Router>
+     <NavLine></NavLine>
+     <Switch>
+       <Route exact path='/'>
+         <Inicio></Inicio>
+       </Route>
+       <Route exact path='/productos'>
+        <ListarProductos></ListarProductos>
+       </Route>
+       <Route exact path='/productos/nuevo'>
+        <AgregarProducto></AgregarProducto>
+       </Route>
+     </Switch>
+     <Footer></Footer>
+   </Router>
   );
 }
 
---
+++ src/components/common/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light text-center py-5 '>
+           <h5> &copy; Todos los derechos reservados.</h5>
+        </div>
+    );
+};
+
+export default Footer;
